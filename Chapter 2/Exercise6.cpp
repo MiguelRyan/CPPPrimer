f5dfe1ee86ec54a764f64ebb210ab06f91495d27
Miguel Ryan <miguel.a.ryan@gmail.com>
2023-05-27 15:45:30 +0100

    Exercises 6 and 7 added.

--- Chapter 2/Exercise6.cpp
+++ Chapter 2/Exercise6.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+double lightyearToAU(double);
 
 int main(){
-    
+    using namespace std;
+
+    cout << "Enter the number of light years: ";
+    double lightyear;
+    cin >> lightyear;
+    double au;
+    au = lightyearToAU(lightyear);
+
+    cout << lightyear << " light years = " << au << " astronomical units" << endl;
+}
+
+double lightyearToAU(double lightyear){
+    double au;
+    au = lightyear * 63240;
+
+    return au;
 }
--- Chapter 2/Exercise7.cpp
+++ Chapter 2/Exercise7.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Miguel on 27/05/2023.
-//
+#include <iostream>
+void timeDisplay(int, int);
+
+int main(){
+    using namespace std;
+
+    cout << "Enter the number of hours: ";
+    int hours;
+    cin >> hours;
+    cout << "Enter the number of minutes: ";
+    int minutes;
+    cin >> minutes;
+
+    timeDisplay(hours, minutes);
+}
+
+void timeDisplay(int hour, int minute){
+    using namespace std;
+
+    cout << "Time: " << hour << ":" << minute << endl;
+}
